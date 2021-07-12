21d30e529d634f71c03e963f60db5c40b226e028
predallien-dev <predallienw8@gmail.com>
2021-07-12 17:24:14 +0300

    теперь работает!

--- ZabbixWorker.py
+++ ZabbixWorker.py
@@ -1,19 +1,39 @@
 from pyzabbix import ZabbixAPI
+
 # Добавляем список с кредами для мониторинга
 ocod_new = ['https://monitoring.dv.local/', 'm.gerbersgagen', 'KmPO6sssqVDk']
-ocod_old = ['http://10.87.188.76/zabbix','m.gerbersgagen', 'KL29JPMe']
+ocod_old = ['http://10.87.188.76/zabbix', 'm.gerbersgagen', 'KL29JPMe']
 
 
-class ZabbixWorker(host):
+class ZabbixWorker:
     """Класс, реализующий ряд функций для удобной работы с Zabbix API"""
-    # Функция принимает на вход номер группы и возвращает id всех хостов в этой группе
 
-    def host_copy(host_creds, groupid):
-        url = host_creds[0]
-        user = host_creds[1]
-        password = hostcreds[2]
+    # Получаем все имена хостов в группе, номер которой передаем функции на вход.
+    # Для этого, сперва вызываем функцию, которая достает id хоста, потом запрашиваем hostname, передавая id
+    def get_hosts_ids(host_creds, groupid):
+        with ZabbixAPI(url=host_creds[0], user=host_creds[1], password=host_creds[2]) as zapi:
+            # get hostnames and ID's of hosts
+            host_ids_and_names = zapi.host.get(groupids=groupid, output=['host'])
+            host_list = []
+
+            # from list of ID's and host, get hostid only
+            for i in range(len(host_ids_and_names)):
+                host_ids = host_ids_and_names[i].get('hostid')
+                host_list.append(host_ids)
+            return host_list
+
+    def get_hosts_names(host_creds, groupid):
+        ZabbixWorker.get_hosts_ids(host_creds, groupid)
+        with ZabbixAPI(url=host_creds[0], user=host_creds[1], password=host_creds[2]) as zapi:
+            host_ids_and_names = zapi.host.get(hostids=ZabbixWorker.get_hosts_ids(host_creds, groupid), output=['host'])
+            host_list = []
+            for i in range(len(host_ids_and_names)):
+                host_list.append(host_ids_and_names[i].get('host'))
+            return host_list
+
+    def host_copy(sourse_host_creds, target_host_creds, host_list, target_groupid):
         for hostname in host_list:
-            with ZabbixAPI(url=host_creds[0], user=host_creds[1], password=hostcreds[2]) as zapi:
+            with ZabbixAPI(url=sourse_host_creds[0], user=sourse_host_creds[1], password=sourse_host_creds[2]) as zapi:
                 original_host = zapi.host.get(
                     filter={'host': hostname},
                     selectGroups='extend',
@@ -27,125 +47,15 @@ class ZabbixWorker(host):
                     name=original_host['name'] + ' (history)'
                 )
 
-                with ZabbixAPI(url=host_creds[0], user=host_creds[1], password=hostcreds[2]) as zapi:
+                with ZabbixAPI(url=target_host_creds[0], user=target_host_creds[1],
+                               password=target_host_creds[2]) as zapi:
                     clone = zapi.host.create(
                         host=original_host['host'],
                         name=original_host['name'],
                         proxy_hostid=original_host['proxy_hostid'],
-                        groups=[{'groupid': groupid}],
+                        groups=[{'groupid': target_groupid}],
                         macros=original_host['macros'],
                         interfaces=[
                             {'main': '1', 'type': '1', 'useip': '1', 'dns': '', 'port': '10050', 'bulk': '1',
                              'ip': original_host['interfaces'][0]['ip']}])
-                    print('copy host ' + hostname)
-
-# получаем имена хостов в группе, номер которой передаем функции на вход.
-# Для этого, сперва выполняем кусок кода выше, потом добавляем в лист все имена хостов
-# получать сперва id необходимо по причине того, что хостнеймы в группе заббикс
-# просто так не отдает, только по id хоста
-
-def get_hosts_names(groupid):
-
-        def get_hosts_ids(groupid):
-            with ZabbixAPI(url=host_creds[0], user=host_creds[1], password=hostcreds[2]) as zapi:
-                # get hostnames and ID's of hosts
-                host_ids_and_names = zapi.host.get(groupids=groupid, output=['host'])
-                host_list = []
-
-                # from list of ID's and host, get hostid only
-                for i in range(len(host_ids_and_names)):
-                    host_ids = host_ids_and_names[i].get('hostid')
-                    host_list.append(host_ids)
-                return host_list
-
-        with ZabbixAPI(url=host_creds[0], user=host_creds[1], password=hostcreds[2]) as zapi:
-            # get hostnames and ID's of hosts
-            host_ids_and_names = zapi.host.get(hostids=get_hosts_ids(groupid=groupid), output=['host'])
-            host_list = []
-            for i in range(len(host_ids_and_names)):
-                host_list.append(host_ids_and_names[i].get('host'))
-            return host_list
-
-def get_hosts_ids(url, user, password, groupid):
-    with ZabbixAPI(url=target_url, user=user, password=password) as zapi:
-        # get hostnames and ID's of hosts
-        host_ids_and_names = zapi.host.get(groupids=groupid, output=['host'])
-        host_list = []
-
-        # from list of ID's and host, get hostid only
-        for i in range(len(host_ids_and_names)):
-            host_ids = host_ids_and_names[i].get('hostid')
-            host_list.append(host_ids)
-        return host_list
-    # начиная отсюда не тестировалось!
-    # получаем ip хостов в группе 25
-    '''
-    def get_hosts_ips(self):
-        with ZabbixAPI(url=target_url, user=user, password=password) as zapi:
-            ip_retrieve = zapi.hostinterface.get(hostids=get_hosts_ids, output=['ip'])
-            ip_list = []
-            i = 0
-            while i < len(ip_retrieve):
-                ip_addr = ip_retrieve[i].get('ip')
-                ip_list.append(ip_addr)
-                i += 1
-            return(ip_list)
-
-    # добавляем SNMP интерейс на всех хосты группы 25
-    # SNMP ip = Agent IP
-
-    def add_snmp(self):
-        i = 0
-        with ZabbixAPI(url=target_url, user=user, password=password) as zapi:
-            while i < len(get_hosts_ips()):
-                print(i)
-                try:
-                    snmp_add = zapi.hostinterface.create(
-                            dns='',
-                            hostid=get_hosts_list()[i],
-                            ip=get_hosts_ips()[i],
-                            main='0',
-                            port='161',
-                            type='2',
-                            useip='1',
-                            details={
-                                'version': '2',
-                                'bulk': '1',
-                                'community': '($SNMP_COMMUNITY)'
-                            }
-
-                        )
-
-                except:
-                    snmp_add = zapi.hostinterface.create(
-                        dns='',
-                        hostid=get_hosts_list()[i],
-                        ip=get_hosts_ips()[i],
-                        main='1',
-                        port='161',
-                        type='2',
-                        useip='1',
-                        details={
-                            'version': '2',
-                            'bulk': '1',
-                            'community': '($SNMP_COMMUNITY)'
-                        }
-                    )
-
-                i += 1
-
-    def add_template(self):
-
-        with ZabbixAPI(url=target_url, user=user, password=password) as zapi:
-            i = 0
-            while i < len(get_hosts_list()):
-                print(i)
-
-                template = zapi.template.massadd(
-                    templates=[{'templateid':'10256'}],
-                    hosts=[{'hostid': get_hosts_list()[i]}]
-                )
-                i += 1
-
-
-'''
+                    print('Copying host ' + hostname)
